ef27025cd4d453bb9c83d62430119cafd7c2669e
Brivno1234 <seredaivan2004@gmail.com>
2025-05-07 09:45:38 +0300

    abdete file

--- Ckripts.py
+++ Ckripts.py
@@ -1,16 +1,44 @@
 import os
 import time
 
+import csv
+import datetime
+
+
+# сохранить в вайлик резульнаты пинга
+
 hosts= ["8.8.8.8", "1.1.1.1", "192.168.1.1"]
+now = datetime.datetime.now()
+print(now)
+#Time.status
+#05.06.20.25 ok
+#05.06.20.25,Fail
+
+with open("ping_log.csv","w") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Time","Status"])
+
+
 
 while True:
     print("Kättesadavuse kontroll")
+    now = datetime.datetime.now()
+    result = ""
     for elem in hosts:
         response =os.system(f"ping -n 1 {elem} > null")
         if response == 0:
+            resalt = "ok"
             print(elem, "kätesadavalt")
         else:
+            resalt = "fail"
             print(elem ," ei ole kättesadavalt")
+            
+        with open("ping_log.csv","a") as file:
+            writer = csv.writer(file)
+            writer.writerow([ now,result ])            
+            
+            
+            
         print("-"*30)
-        time.sleep(5)
+        time.sleep(0)
         
